211ecae0f41f71440dae7b9f2b57203be704014e
binginx <sdaulibin@126.com>
2023-01-04 17:06:47 +0800

    rust study

--- function/target/debug/deps/function-1a95e8c9243dc4c1.d
+++
@@ -1,5 +0,0 @@
-/Users/binginx/rust/rust-study/function/target/debug/deps/function-1a95e8c9243dc4c1.rmeta: src/main.rs
-
-/Users/binginx/rust/rust-study/function/target/debug/deps/function-1a95e8c9243dc4c1.d: src/main.rs
-
-src/main.rs:
--- function/target/debug/deps/function-34d475f3e53f3b74.d
+++
@@ -1,5 +0,0 @@
-/Users/binginx/rust/rust-study/function/target/debug/deps/function-34d475f3e53f3b74.rmeta: src/main.rs
-
-/Users/binginx/rust/rust-study/function/target/debug/deps/function-34d475f3e53f3b74.d: src/main.rs
-
-src/main.rs:
