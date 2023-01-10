cbe2581873c952cf9004e3a1dcdd3001b99347dc
binginx <sdaulibin@126.com>
2023-01-10 11:11:17 +0800

    rust study panic

--- exception/src/main.rs
+++ exception/src/main.rs
@@ -1,10 +1,14 @@
-use std::{fs::File, io::ErrorKind};
+use std::fs::File;
+use std::io;
+use std::io::Read;
+use std::io::ErrorKind;
 
 fn main() {
     println!("Hello Word");
     // panic!("crash & burn");
     // first();
     //let file = File::open("hello.md");
+    //let file = File::open("hello.md").expect("无法打开文件");
     // match file {
     //     Ok(file) => file,
     //     Err(error) => match error.kind() {
@@ -29,16 +33,30 @@ fn main() {
     });
 }
 
-fn first() {
-    second();
-}
+// fn first() {
+//     second();
+// }
 
-fn second() {
-    third(33);
-}
+// fn second() {
+//     third(33);
+// }
  
-fn third(num: i32) {
-    if num == 33 {
-        panic!("请不要输入33");
+// fn third(num: i32) {
+//     if num == 33 {
+//         panic!("请不要输入33");
+//     }
+// }
+
+fn read_username_from_file() -> Result<String, io::Error>{
+    let f = File::open("hello.txt");
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+    match  f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
     }
 }
