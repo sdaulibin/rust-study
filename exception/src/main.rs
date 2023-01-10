feb8a499ebcac680e7bd5f98366b14be41062167
binginx <sdaulibin@126.com>
2023-01-10 11:33:13 +0800

    rust study panic

--- exception/src/main.rs
+++ exception/src/main.rs
@@ -7,20 +7,20 @@ fn main() {
     println!("Hello Word");
     // panic!("crash & burn");
     // first();
-    //let file = File::open("hello.md");
-    //let file = File::open("hello.md").expect("无法打开文件");
-    // match file {
-    //     Ok(file) => file,
-    //     Err(error) => match error.kind() {
-    //         ErrorKind::NotFound => match File::create("hello.txt") {
-    //             Ok(fc) => fc,
-    //             Err(error) => panic!("无法创建文件:{:?}",error),
-    //         },
-    //         other_error => {
-    //             panic!("无法打开文件: {:?}",error)
-    //         }
-    //     },
-    // };
+    /*let file = File::open("hello.md");
+    let file = File::open("hello.md").expect("无法打开文件");*/
+    /* match file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(error) => panic!("无法创建文件:{:?}",error),
+            },
+            other_error => {
+                panic!("无法打开文件: {:?}",error)
+            }
+        },
+    };*/
     //闭包
     let f = File::open("hello.txt").unwrap_or_else(|error| {
         if error.kind() == ErrorKind::NotFound {
@@ -33,19 +33,20 @@ fn main() {
     });
 }
 
-// fn first() {
-//     second();
-// }
+//测试RUST_BACKTRACE=1 cargo run
+/* fn first() {
+    second();
+}
 
-// fn second() {
-//     third(33);
-// }
+fn second() {
+    third(33);
+}
  
-// fn third(num: i32) {
-//     if num == 33 {
-//         panic!("请不要输入33");
-//     }
-// }
+fn third(num: i32) {
+    if num == 33 {
+        panic!("请不要输入33");
+    }
+}*/
 
 fn read_username_from_file() -> Result<String, io::Error>{
     let f = File::open("hello.txt");
