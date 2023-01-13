a5682f1cf3afd4763e541b55119ff6d098950663
binginx <sdaulibin@126.com>
2023-01-13 22:44:45 +0800

    rust study traits

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -67,7 +67,7 @@ impl<T:Display + PartialOrd> Pair<T> {
 }*/
 
 //Trait Bound 泛型 特征限制
-pub fn notify<T: Summary>(item: &T) {
+pub fn notify<T: Summary>(item: &T) gi
     println!("Breaking news! {}",item.summarize())
 }
 
