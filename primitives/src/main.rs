377a8eac3334f62a1f97fb34d827dc860df6bc59
binginx <sdaulibin@126.com>
2023-01-16 10:08:05 +0800

    rust study >> primtives

--- primitives/src/main.rs
+++ primitives/src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{fmt, mem};
 //元组可以充当函数的参数和返回值
 fn reverse(pair: (i32,bool)) -> (bool,i32) {
     //可以使用`let`把元组的成员绑定到一些变量
@@ -17,6 +17,11 @@ impl fmt::Display for Matrix {
 fn transpose(matrix: Matrix) -> Matrix{
     let result = Matrix(matrix.0,matrix.3,matrix.2,matrix.1);
     result
+}
+
+fn analyze_slice(slice: &[i32]) {
+    println!("first element of the slice: {}",slice[0]);
+    println!("ths size has {} element",slice.len());
 }
 fn main() {
     // 整数相加
@@ -76,4 +81,31 @@ fn main() {
      
     println!("Matrix:\n{}", matrix);
     println!("Transpose:\n{}", transpose(matrix));
+
+    // 定长数组（类型标记是多余的）
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // 所有元素可以初始化成相同的值
+    let ys: [i32; 500] = [0; 500];
+
+    // 下标从 0 开始
+    println!("first element of the array: {}", xs[0]);
+    println!("second element of the array: {}", xs[1]);
+
+    // `len` 返回数组的大小
+    println!("array size: {}", xs.len());
+
+    // 数组是在栈中分配的
+    println!("array occupies {} bytes", mem::size_of_val(&xs));
+
+    // 数组可以自动被借用成为 slice
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs);
+
+    // slice 可以指向数组的一部分
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&ys[1 .. 4]);
+
+    // 越界的下标会引发致命错误（panic）
+    //println!("{}", xs[5]);
 }
