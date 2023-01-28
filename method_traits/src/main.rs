522f5e78cbf2897ba3b7d37cbddea59dd692019a
binginx <sdaulibin@126.com>
2023-01-28 14:34:51 +0800

    rust study >> method and trait

--- method_traits/src/main.rs
+++ method_traits/src/main.rs
@@ -45,6 +45,33 @@ impl Animal for Sheep {
     }
 }
 
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Point {
+    fn origin() -> Point {
+        Point { x: 0.0, y: 0.0 }
+    }
+
+    fn new(x:f64,y :f64) ->Point {
+        Point { x: x, y: y }
+    }
+}
+
+struct Rectangle {
+    p1 : Point,
+    p2 : Point,
+}
+
+impl Rectangle {
+    fn area(&self) -> f64 {
+        let Point { x: x1, y: y1} = self.p1;
+        let Point { x: x2, y: y2} = self.p2;
+        ((x1 - x2) * (y1 - y2)).abs()
+    }
+}
 fn main() {
     println!("Hello, world!");
     let mut dolly: Sheep = Animal::new("Dolly".to_string());
